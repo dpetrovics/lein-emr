033b419b0a050515130135dcaeefa9dd98e86077
David Petrovics <dpetrovics@gmail.com>
2012-09-06 15:52:07 -0400

    lein-emr is now operational! still more work to be done, need to take out all the config stuff so its a general lib

--- project.clj
+++ project.clj
@@ -2,8 +2,11 @@
   :description "Wrapper over AWS elastic-mapreduce ruby client. Used
   as a lein plugin to create job flows using Amazon Elastic
   MapReduce."
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/tools.cli "0.2.2"]]
+  :url "https://github.com/dpetrovics/lein-emr"
+  :repositories {"sonatype"
+                 "http://oss.sonatype.org/content/repositories/releases"}
+  :dependencies [[org.cloudhoist/pallet "0.7.2"]
+                 [org.clojure/tools.cli "0.2.2"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- src/leiningen/emr.clj
+++ src/leiningen/emr.clj
@@ -1,6 +1,9 @@
 (ns leiningen.emr
-  (:use clojure.tools.cli))
+  (:use clojure.tools.cli
+        [clojure.string :only (join)])
+  (:require [pallet.execute :as execute]))
 
+;;HELPER FUNCTIONS
 (defn has [n xs]
   (= n (count xs)))
 
@@ -31,6 +34,124 @@
   `(fn [arg-map#]
      (-> arg-map# ~@validators)))
 
+;;CONFIG STUFF THAT NEEDS TO GO ELSEWHERE
+(def bootstrap-actions
+  ["s3://elasticmapreduce/bootstrap-actions/configurations/latest/memory-intensive"
+   "s3://elasticmapreduce/bootstrap-actions/add-swap --args 2048"
+   "s3://elasticmapreduce/bootstrap-actions/configure-hadoop --args FIXTHIS"
+   "s3://reddconfig/bootstrap-actions/forma_bootstrap_robin.sh"])
+
+(def native-path "/home/hadoop/native")
+(def lib-path "/usr/local/fwtools/usr/lib")
+(def redd-config-path "s3://reddconfig/bootstrap-actions/config.xml")
+
+(def default-bid-price
+  {"large" 0.32
+   "cluster-compute" 1.30
+   "high-memory" 1.80})
+
+(defn calc-maps-reds
+  "Given the type, it will return a map of the number of map tasks and
+  reduce tasks using sensible defaults."
+  [type mappers reducers]
+  (cond
+   (= type "large")             {:map-tasks (or mappers 4)
+                                 :reduce-tasks (or reducers 2)}
+   (= type "high-memory")       {:map-tasks (or mappers 30)
+                                 :reduce-tasks (or reducers 24)}
+   (= type "cluster-compute")   {:map-tasks (or mappers 22)
+                                 :reduce-tasks (or reducers 16)}))
+
+(defn convert-type
+  [type]
+  (cond
+   (= type "large") "m1.large"
+   (= type "high-memory") "m2.4xlarge"
+   (= type "cluster-compute") "cc1.4xlarge"))
+
+;;put this into an XML file in redd-config path, except for the ones
+;;that depend on reduce tasks, map tasks, and node countx
+(defn base-props
+  [reduce-tasks map-tasks node-count]
+  {:hdfs-site {:dfs.datanode.max.xcievers 5096
+               :dfs.namenode.handler.count 20
+               :dfs.block.size 134217728
+               :dfs.support.append true}
+   :mapred-site {:io.sort.mb 200
+                 :io.sort.factor 40
+                 :mapred.reduce.parallel.copies 20
+                 :mapred.task.timeout 10000000
+                 :mapred.reduce.tasks (int (* reduce-tasks node-count))
+                 :mapred.tasktracker.map.tasks.maximum map-tasks
+                 :mapred.tasktracker.reduce.tasks.maximum reduce-tasks
+                 :mapred.reduce.max.attempts 12
+                 :mapred.map.max.attempts 20
+                 :mapred.job.reuse.jvm.num.tasks 20
+                 :mapred.map.tasks.speculative.execution false
+                 :mapred.reduce.tasks.speculative.execution false
+                 :mapred.output.direct.NativeS3FileSystem true
+                 :mapred.child.java.opts (str "-Djava.library.path="
+                                              native-path
+                                              " -Xms1024m -Xmx1024m")
+                 :mapred.child.env (str "LD_LIBRARY_PATH="
+                                        lib-path)}})
+
+(defn parse-emr-config
+  "Takes in a config map of hadoop base props (config file settings)
+  and returns a string of hadoop properties for use by the ruby
+  elastic-mapreduce script."
+  [conf-map]
+  (->> (mapcat conf-map [:mapred-site :hdfs-site])
+       (map (fn [[k v]]
+              (format "-s,%s=%s" (name k) v))) 
+       (join ",")       
+       (format "\"--core-config-file,%s,%s\"" redd-config-path)))
+
+
+;;SCRIPT GENERATION
+(defn boot-emr!
+  [{:keys [name type size zone mappers reducers bid on-demand bs-actions] :as m}]
+  (let [{:keys [map-tasks reduce-tasks]}
+        (calc-maps-reds type mappers reducers)
+        hw-id (convert-type type)]
+    (execute/local-script
+     (elastic-mapreduce --create --alive
+                        --name ~name
+                        --availability-zone ~zone
+                        --ami-version "2.0.5" ;;not sure if we still
+                        ;;need this
+                        
+                        --instance-group master
+                        --instance-type ~hw-id
+                        --instance-count 1 
+                        
+                        --instance-group core
+                        --instance-type ~hw-id
+                        --instance-count ~size
+                        ~(if (nil? bid)
+                           (if (nil? on-demand)
+                             ;;use default bid price
+                             (str "--bid-price "
+                                  (default-bid-price type)) 
+                             "")                    ;;use on-demand
+                           (str " --bid-price " bid)) ;;use given price
+                        --enable-debugging
+
+                        --bootstrap-action ;;MAKE THIS INTO A LOOP
+                        s3://elasticmapreduce/bootstrap-actions/configurations/latest/memory-intensive
+                        
+                        --bootstrap-action
+                        s3://elasticmapreduce/bootstrap-actions/add-swap
+                        --args 2048
+                        
+                        --bootstrap-action
+                        s3://elasticmapreduce/bootstrap-actions/configure-hadoop
+                        --args ~(parse-emr-config (base-props map-tasks reduce-tasks size))
+
+                        --bootstrap-action
+                        s3://reddconfig/bootstrap-actions/forma_bootstrap_robin.sh))))
+
+;;VALIDATORS
 (defn size-valid?
   "This step checks that, if size exist in the arg map,
   they're accompanied by a valid size. If this passes, the function acts as
@@ -45,7 +166,7 @@
   arg map, they're accompanied by a valid bid-price. If this passes,
   the function acts as identity, else an error is added to the map."
   [{:keys [bid] :as m}]
-  (if (nil? bid)
+  (if (and (contains? m :bid) (nil? bid))
     (add-error m "Please provide a valid bid price (must be a number).")
     m))
 
@@ -86,6 +207,7 @@
    (bid-or-ondemand?)
    (bidprice-valid?)))
 
+;;PARSING CLI ARGS
 (defn parse-hadoop-args
   "Used to parse command line arguments.  Returns a vector containing
   a map of the parsed arguments, a vector of extra arguments that did
@@ -102,7 +224,7 @@
   (try
     (cli args
          ["-n" "--name" "Name of cluster." :default "dev"]
-         ["-t" "--type" "Type  cluster."]
+         ["-t" "--type" "Type  cluster." :default "high-memory"]
          ["-s" "--size" "Size of cluster." :parse-fn #(try
                                                         (Long. %)
                                                         (catch Exception _
@@ -145,5 +267,4 @@
       (let [val-arg-map (hadoop-validator (first arg-map))]
         (if-let [e-seq (:_errors val-arg-map)]
           (print-errors e-seq) ;;if validation contains error
-          (println "map after validation:  " val-arg-map) ;;no error
-          )))))
+          (boot-emr! val-arg-map))))))
